309ec99fb77678c8fa949f49465e34d92767eb6b
Egor Duraev <egduraev1997@gmail.com>
2023-12-04 22:23:04 +0300

    Homework

--- HW_4.cs
+++ HW_4.cs
@@ -10,17 +10,17 @@
 // Task 1
 string str;
 int number;
-Console.Write("Введите целое число:");
+Console.Write("Введите целое число: ");
 do {
     str = Console.ReadLine();
     if (str == "q") {
-        Console.WriteLine("Выход из программы");
+        Console.WriteLine("[STOP]");
         break;
     }
 if(!int.TryParse(str, out number))
     Console.WriteLine("Нужно вводить целое число"); else {
         if ((number % 2) == 0) {
-            Console.WriteLine("Выход из программы");
+            Console.WriteLine("[STOP]");
             break;
         }
     }
@@ -31,31 +31,35 @@ if(!int.TryParse(str, out number))
 Random rnd = new Random();
 int[] array = new int[10];
 int counter = 0;
+Console.Write("[ ");
 for (int i = 0; i < 10; i++) {
     array[i] = rnd.Next(99, 1000);
     if ((array[i] % 2) == 0) counter++;
     Console.Write(array[i] + " ");
 }
-Console.WriteLine("\nКоличество четных чисел: " + counter);
+Console.Write("]");
+Console.WriteLine(" => " + counter);
 
 // Task 3
 Console.Write("Введите размер матрицы: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] oneDimArr = new int[size];
-Console.Write("Исходная матрица: ");
+Console.Write("[ ");
 for (int i = 0; i < size; i++) {
     oneDimArr[i] = rnd.Next(0, 1000);
     Console.Write(oneDimArr[i] + " ");
 }
+Console.Write("]");
 int temp = 0;
 for (int i = 0; i < size/2; i++) {
     temp = oneDimArr[i];
     oneDimArr[i] = oneDimArr[size - 1 - i];
     oneDimArr[size - 1 - i] = temp;
 }
-Console.Write("\nПеревёрнутая матрица: ");
+Console.Write(" => [ ");
 for (int i = 0; i < size; i++) {
     Console.Write(oneDimArr[i] + " ");
 }
+Console.Write("]");
 
     
